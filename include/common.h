6809ac4ab59ddd7f5968ee0c87ff4aced1adff5a
metaphysicser <zpl010720@qq.com>
2023-07-04 14:25:59 +0800

    update copyright

--- include/common.h
+++ include/common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright [2023] [malab Pinglu Zhang]
+ * Copyright [2023] [MALABZ_UESTC Pinglu Zhang]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
